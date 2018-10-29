a967d2d2e73200b1a4cdc9b5229a541a02ccd918
linkinghack <linkinghack@outlook.com>
2018-10-29 13:35:33 +0800

    Complete the pool model

--- ConcurrencyModel/PoolTest/main.go
+++ ConcurrencyModel/PoolTest/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"sync"
 	"io"
 	"log"
+	"math/rand"
+	"sync"
 	"sync/atomic"
+	"time"
+
 	"./pool"
 )
 
+const (
+	maxGoroutines   = 25
+	pooledResources = 2 // number of resources in the pool
+)
+
+type dbConnection struct {
+	ID int32
+}
+
+// Close implements the interface Close{}
+func (dbConn *dbConnection) Close() error {
+	log.Println("Close: Connection", dbConn.ID)
+	return nil
+}
+
+// Generate the ID of Connections
 var idCounter int32
 
+// createConnection is the Factory of dbConnection
 func createConnection() (io.Closer, error) {
 	id := atomic.AddInt32(&idCounter, 1)
 	log.Println("Create: New Connection", id)
@@ -17,12 +37,38 @@ func createConnection() (io.Closer, error) {
 	return &dbConnection{id}, nil
 }
 
-var wg sync.WaitGroup
-wd.Add(maxGoroutines)
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(maxGoroutines)
 
-p, err := pool.New(createConnection, pooledResources)
-if err != nil {
-	log.Println(err)
+	p, err := pool.New(createConnection, pooledResources)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for query := 0; query < maxGoroutines; query++ {
+		go func(q int) {
+			performQueries(q, p) // there is a closure, access 'p' which created out of the go func
+			wg.Done()
+		}(query)
+	}
+
+	wg.Wait()
+
+	log.Println("Shutdown Program.")
+	p.Close()
 }
 
-for query := 0;
+func performQueries(query int, p *pool.Pool) {
+	conn, err := p.Acquire()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer p.Release(conn)
+
+	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	log.Printf("QID[%d] CID[%d]\n", query, conn.(*dbConnection).ID) // type cast of interface
+
+}
